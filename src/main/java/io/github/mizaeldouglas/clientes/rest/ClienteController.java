628e55c18cae345b8b1bca9bdd46e6d8c74c1799
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-09-02 11:14:14 -0300

    Obtendo as informações de um Cliente

--- src/main/java/io/github/mizaeldouglas/clientes/rest/ClienteController.java
+++ src/main/java/io/github/mizaeldouglas/clientes/rest/ClienteController.java
@@ -5,10 +5,8 @@ import io.github.mizaeldouglas.clientes.model.entity.Cliente;
 import io.github.mizaeldouglas.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -23,8 +21,17 @@ public class ClienteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(Cliente cliente) {
+	public Cliente salvar(@RequestBody Cliente cliente) {
 		return repository.save(cliente);
 	}
+
+	@GetMapping("{id}")
+	public Cliente acharPorId(@PathVariable Integer id){
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+	}
+
 
 }
